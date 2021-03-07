39507a6d5c014eab863e9291a31fba321ccce267
Mahdiyeh <mahdiyeh.fani@gmail.com>
2021-03-07 17:35:46 +0330

    add project to git

---
+++ src/components/NavbarBooks.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from "reactstrap";
+
+const Example = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">WordBook</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="me-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Books</NavLink>
+            </NavItem>
+
+            <NavItem>
+              <NavLink href="/components/">Article</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Example;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { Button, Form, FormGroup, Label, Input, Container } from "reactstrap";
+import CardsBooks from "./CardsBooks";
+
+const SearchBox = () => {
+  const [books, setBooks] = useState([]);
+  const [query, setQuery] = useState();
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [maxResults, setMaxResults] = useState(10);
+
+  const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}`;
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    console.log(query);
+    setSearch("");
+  };
+
+  useEffect(() => {
+    getBooks();
+  }, [query]);
+
+  const getBooks = async () => {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    setBooks(data.itmes);
+
+    console.log(data.items);
+  };
+
+  return (
+    <div>
+      <div className="mainImage d-flex justify-content align-items-center flex-column">
+        <h3 className="display-2 text-center  mb-2" style={{ zIndex: 2 }}>
+          Shellfs books
+        </h3>
+        <div>
+          <Form inline onSubmit={getSearch}>
+            <FormGroup>
+              <Input type="text" value={search} onChange={updateSearch} />
+            </FormGroup>
+            <Button type="submit">Search</Button>
+          </Form>
+        </div>
+      </div>
+      <CardsBooks />
+    </div>
+  );
+};
+
+export default SearchBox;
